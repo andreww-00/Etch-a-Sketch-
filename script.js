b111bd2aeabe794c6bd30cfe705825e1c32af5a8
Andrew <a.lucas.webster@gmail.com>
2023-02-22 23:26:08 -0500

    added hover effect

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 let container = document.querySelector(".grid_container");
 
 function makeGrid(gridSize) {
-    //need to create a row of size grid size 
+
     container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-   
-    //then create column of grid size
+
     for(i=0; i < gridSize * gridSize; i++) {
         let cell = document.createElement("div");
         container.appendChild(cell);
     }
 }
 
-makeGrid(7);
+makeGrid(4);
+
+//responsible for hover draw effect
+let ind = container.querySelectorAll("div");
+ind.forEach(item => {
+    item.addEventListener("mouseenter", function(e) {
+        let change = e.target;
+        change.style.backgroundColor = "black";
+    });
+});
 
 
-        
+
+
+
+
+
