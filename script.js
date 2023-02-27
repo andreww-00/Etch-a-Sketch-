e7992214502820d3caa568baf27c27e4f47e6495
Andrew <a.lucas.webster@gmail.com>
2023-02-27 17:20:18 -0500

    added grid sizing input and moved code around

--- script.js
+++ script.js
@@ -4,27 +4,46 @@ function makeGrid(gridSize) {
 
     container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-
     for(i=0; i < gridSize * gridSize; i++) {
         let cell = document.createElement("div");
         container.appendChild(cell);
+        listen();
     }
 }
 
-makeGrid(4);
+makeGrid(10);
 
-//responsible for hover draw effect
-let ind = container.querySelectorAll("div");
-ind.forEach(item => {
-    item.addEventListener("mouseenter", function(e) {
-        let change = e.target;
-        change.style.backgroundColor = "black";
+//responsible for adding listeners and hover draw effect
+function listen() {
+    let ind = container.querySelectorAll("div");
+    ind.forEach(item => {
+        item.addEventListener("mouseenter", function(e) {
+            let change = e.target;
+            change.style.backgroundColor = "black";
+        });
     });
+}
+
+//grid sizing
+let button = document.querySelector(".grid_Sizing");
+button.addEventListener("click", (e) => {
+    let size_store = prompt("Input grid size!");
+    if (size_store <= 20) {
+        clear();
+        makeGrid(size_store);
+    }
+    else {
+        alert("please enter value less than 50!")
+    }
+    
 });
 
 
-
-
+//clears grid
+function clear() {
+    let ind = container.querySelectorAll("div");
+    ind.forEach((item) => item.remove());
+}
 
 
 
